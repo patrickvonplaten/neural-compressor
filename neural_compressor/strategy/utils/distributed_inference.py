1531fb2285b7284272440ff4e9f1a414ca5571bd
yiliu30 <yi4.liu@intel.com>
2022-12-19 18:46:24 +0800

    refine the conponment
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- neural_compressor/strategy/utils/distributed_inference.py
+++ neural_compressor/strategy/utils/distributed_inference.py
@@ -3,22 +3,59 @@ import math
 import time
 import random
 from copy import deepcopy
+from typing import List, Dict
 
-# @ray.remote
-# class ResultMonitor:
-#     def __init__(self) -> None:
-#         print(f"[ResultMonitor] Initializing a new ResultMonitor.")
-#         pass
-    
-#     def get_result(self, trial_id, result):
-#         print(f"")
-#         return trial_id, result
+import ray
+@ray.remote
+class ResultMonitor:
+    def __init__(self) -> None:
+        """Class for monitor all trials state and report the evaluation result.
 
+        The completed trial: the trial has been completed but not reported to strategy.
+        The reported trial: the trial has been completed and reported to strategy.
+        """
+        self.trials_lst = []
+        self.completed_trials = []
+        self.reported_trials = set()
+
+    def all_trials_reported(self):
+        """Check if all trials are completed and reported.
+
+        Returns:
+            result(bool): the check result. True for all completed and reported trials, False for none.
+        """
+        return len(self.trials_lst) == len(self.reported_trials)
+
+    def report_results(self):
+        """Collect all completed but not reported trails at current state.
+
+        Returns:
+            result_record (Dict): the result collection. key: trial id, val : evaluation result.
+        """
+        result_record = []
+        for trial in self.completed_trials():
+            if trial not in self.reported_trials:
+                result_record.append({trial.id: trial.result()})
+        return result_record
+
+    def add_finished_trial(self):
+        """Trial can report its result by this interface.
+
+        Returns:
+            None
+        """
+        pass
 
 @ray.remote
 class Trial:
-    def __init__(self, tune_cfg) -> None:
+    def __init__(self, tune_cfg, adaptor, model, calib_dataloader, q_func, evaluate, result_monitor: ResultMonitor) -> None:
+        """Class to run one independent task including calibration, quantization and evaluation.
+
+        Args:
+
+        """
         print(f"[Trial] Initializing a new Trial {id(self)} with tune cfg: {id(tune_cfg)}.")
+        self.result_monitor = result_monitor
         self.tune_cfg = tune_cfg
         # self.adaptor = adaptor
         # self.eval = eval
@@ -26,34 +63,84 @@ class Trial:
         self.finished_trial = False
         pass
 
-    def compute(self, tune_cfg, adaptor, model, calib_dataloader, q_func, evaluate):
+    def compute(self):
+        """Execute one trial including calibration, quantization and evaluation. 
+
+        Args:
+            tune_cfg: _description_
+
+        Returns:
+            result(Dict):
+                q_model: the quantized model.
+                eval_result: the evaluation result.
+        """
+        result = {}
         print(f"[Trial] Start to compute precess for one trial.")
-        q_modle = adaptor.quantize(tune_cfg, model, calib_dataloader, q_func)
-        eval_result = evaluate(q_modle)
+        q_modle = self.adaptor.quantize(self.tune_cfg, self.model, self.calib_dataloader, self.q_func)
+        eval_result = self.evaluate(q_modle)
         print(f"[Trial] Finished the compute with eval result {eval_result}.")
         self.eval_result = eval_result
         self.finished_trial = True
+        # report tot result monitor 
+        self.result_monitor.add_finished_trial.remote(result)
         return eval_result
-    
+
     def report_state(self):
         print(f"[Trial] Report state for {id(self)}.")
         return self.finished_trial
     
-    def report_eval_result(self):
+    def report_result(self):
         print(f"[Trial] Report eval result for {id(self)}.")
         return self, self.eval_result
+    
 
+@ray.remote
 class Scheduler:
-    def __init__(self) -> None:
+    def __init__(self, result_monitor: ResultMonitor) -> None:
+        """Class for manage the hardware resource and dispatch trials.
+        """
         ray.init(address="local")
         print(f"[Scheduler] Initializing a new scheduler.")
+        self.result_monitor = result_monitor
         self.trials_lst = []
-        pass
 
-    def dispatch_trails(self, tune_cfg_lst, adaptor, model, calib_dataloader, q_func, evaluate):
-        for tune_cfg in tune_cfg_lst:
+    def trial_lst(self) -> List[Trial]:
+        return self.trials_lst
+
+    def dispatch_trails(self):
+        """Dispatch the trial to remote actor.
+        """
+        for tune_cfg in self.tune_cfg_lst:
+            # TODO check the hardware resource before schedule new trial
             print(f"[Scheduler] Create a new trial for the {id(tune_cfg)}.")
-            trial = Trial.remote(tune_cfg)
+            trial = Trial.remote(tune_cfg, self.result_monitor)
             self.trials_lst.append(trial)
             trial.compute.remote(tune_cfg, adaptor, model, calib_dataloader, q_func, evaluate)
+
+class DistributedRunner:
+    def __init__(self, tune_cfg_lst, agrs) -> None:
+        """Initialize the components for distributed tuning.
+
+        Args:
+            tune_cfg_lst: _description_
+            agrs: _description_
             
+        Example:
+            runner = DistributedRunner(tune_cfg_lst=[1, 2, 3], args = None)
+            for result in runner.next_result:
+                yield result
+        """
+        self.result_monitor = ResultMonitor()
+        self.scheduler = Scheduler(result_monitor=self.result_monitor, *agrs)
+        self.scheduler.dispatch_trails()
+
+    def next_result(self):
+        """
+        Collect all completed trials and report it to strategy.
+        
+        """
+        while not self.result_monitor.all_trials_reported():
+            for result in self.result_monitor.report_results:
+                yield result
+            # TODO may need to find a better way.
+            time.sleep(5) # pause 5s to do next query. 
